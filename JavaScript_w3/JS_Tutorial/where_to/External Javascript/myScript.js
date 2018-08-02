3feb367c719d1d3382ab70c9d25ec9c9da259a54
yinmazatin <yinmazatin21@gmail.com>
2018-08-02 21:24:52 +0700

    Learn JS from w3 School
    Learn JS from w3 School in Description

---
+++ JavaScript_w3/JS_Tutorial/where_to/External Javascript/myScript.js
@@ -0,0 +1,12 @@
+//External scripts cannot contain <script> tags.
+
+
+// Execute the first then come to execute the below
+function myFunction() {
+   document.getElementById("demo").innerHTML = "Paragraph changed EXTERNAL_2.";
+}
+
+function myFunction() {
+   document.getElementById("demo").innerHTML = "Paragraph changed EXTERNAL.";
+}
+
